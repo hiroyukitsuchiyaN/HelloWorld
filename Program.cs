13c667906cca19abe3cc09b333f0497a34b1cf06
hiroyukitsuchiyaN <82484376+hiroyukitsuchiyaN@users.noreply.github.com>
2021-09-01 16:55:28 +0900

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Prg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
